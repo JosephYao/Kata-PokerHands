d471f8585cb2d7161781b144a4c4dd35c8cd01e6
josephyao <dermott@126.com>
2013-06-01 00:27:41 +0800

    High card with only first card different

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -1,11 +1,21 @@
 
 public class PokerHands implements Comparable<PokerHands> {
 
+	private final String cards;
+
 	public PokerHands(String cards) {
+		this.cards = cards;
 	}
 
-	public int compareTo(PokerHands o) {
+	public int compareTo(PokerHands another) {
+		if (getFirstCardRank(cards) > getFirstCardRank(another.cards))
+			return 1;
+		
 		return 0;
+	}
+
+	private int getFirstCardRank(String cards) {
+		return Integer.parseInt(String.valueOf(cards.charAt(0)));
 	}
 
 }
--- src/test/java/TestPokerHands.java
+++ src/test/java/TestPokerHands.java
@@ -2,14 +2,28 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-
 public class TestPokerHands {
 
 	@Test
 	public void high_card_with_same_cards_are_equal() {
-		PokerHands first = new PokerHands("2S 3D 4H 5C 7S");
-		PokerHands second = new PokerHands("2S 3D 4H 5C 7S");
-		assertEquals(0, first.compareTo(second));
+		assertPokerHandsEquals("2S 3D 4H 5C 7S", "2S 3D 4H 5C 7S");
 	}
 
+	@Test
+	public void high_card_with_only_first_card_different() {
+		assertPokerHandsLargerThan("9S 4H 5C 6S 8C", "2S 4H 5C 6S 8C");
+	}
+
+	private void assertPokerHandsLargerThan(String firstPokerHands, String secondPokerHands) {
+		PokerHands first = new PokerHands(firstPokerHands);
+		PokerHands second = new PokerHands(secondPokerHands);
+		assertEquals(1, Integer.signum(first.compareTo(second)));
+	}
+	
+	private void assertPokerHandsEquals(String firstPokerHands, String secondPokerHands) {
+		PokerHands first = new PokerHands(firstPokerHands);
+		PokerHands second = new PokerHands(secondPokerHands);
+		assertEquals(0, Integer.signum(first.compareTo(second)));
+	}
+	
 }
