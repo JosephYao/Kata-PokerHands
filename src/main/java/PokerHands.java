6343f8c7e7341776c679051b7df0d3365173ef32
josephyao <dermott@126.com>
2013-06-04 00:01:09 +0800

    Add a new case for high card with only one card different The whole test for high card with only one card different is done now

--- src/main/java/PokerHands.java
+++ src/main/java/PokerHands.java
@@ -1,6 +1,8 @@
 
 public class PokerHands implements Comparable<PokerHands> {
 
+	private static final int CARD_INDEX_STEP = 3;
+	private static final int CARD_COUNT = 5;
 	private final String cards;
 
 	public PokerHands(String cards) {
@@ -8,14 +10,15 @@ public class PokerHands implements Comparable<PokerHands> {
 	}
 
 	public int compareTo(PokerHands another) {
-		if (getFirstCardRank(0).compareTo(another.getFirstCardRank(0)) == 0)
-			return getFirstCardRank(3).compareTo(another.getFirstCardRank(3));
+		for (int cardIndex = 0; cardIndex < CARD_COUNT; cardIndex++)
+			if (getFirstCardRank(cardIndex).compareTo(another.getFirstCardRank(cardIndex)) != 0)
+				return getFirstCardRank(cardIndex).compareTo(another.getFirstCardRank(cardIndex));
 		
-		return getFirstCardRank(0).compareTo(another.getFirstCardRank(0));
+		return 0;
 	}
 
 	private Integer getFirstCardRank(int cardIndex) {
-		return Integer.parseInt(String.valueOf(cards.charAt(cardIndex)));
+		return Integer.parseInt(String.valueOf(cards.charAt(cardIndex * CARD_INDEX_STEP)));
 	}
 
 }
--- src/test/java/TestPokerHands.java
+++ src/test/java/TestPokerHands.java
@@ -14,6 +14,7 @@ public class TestPokerHands {
 		assertPokerHandsLargerThan("9S 4H 5C 6S 8C", "2S 4H 5C 6S 8C");
 		assertPokerHandsSmallerThan("2S 4H 5C 6S 8C", "9S 4H 5C 6S 8C");
 		assertPokerHandsLargerThan("2S 9H 5C 6S 8C", "2S 4H 5C 6S 8C");
+		assertPokerHandsLargerThan("2S 4H 9C 6S 8C", "2S 4H 5C 6S 8C");
 	}
 	
 	private void assertPokerHandsSmallerThan(String firstPokerHands, String secondPokerHands) {
